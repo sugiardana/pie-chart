07af4ecf082a978b3a86887a95d828f28f0e7815
I Made Sugi Ardana <sugiardana@gmail.com>
2024-11-30 16:40:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/HousingData.csv')
 df = df.dropna()
@@ -11,19 +11,16 @@ sns.histplot(df['AGE'])
 #plt.show()
 
 #Tampilkan juga histogram plot pada kolom AGE menggunakan library matplotlib, kemudian tambahkan xlabel (Umur) dan ylabel (Jumlah) dengan title (Sebaran Umur Rumah)
-'''
 plt.hist(df['AGE'])
 plt.xlabel('Umur')
 plt.ylabel('Jumlah')
 plt.title('Sebaran Umur Rumah')
 plt.show()
-'''
+
 #Tampilkan pie chart pada kolom "CHAS" menggunakan library matplotlib dengan melakukan perhitungan jumlahnya terlebih dahulu
-'''
 count_chas = df.groupby('CHAS').agg(chas_count=('CHAS', 'count')).reset_index()
 plt.pie(count_chas['chas_count'], labels=count_chas['CHAS'], autopct='%1.1f%%')
 plt.show()
-'''
 
 '''
 st.title("🎈 My new app")
